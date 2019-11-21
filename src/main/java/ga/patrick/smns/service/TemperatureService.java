c171de0e43ddb2ddb5e6746f6af67d0424ba998e
Donat <donutellko@gmail.com>
2019-11-21 16:14:59 +0300

    Add TemperatureService. lastInputs method to get N last inputs.

---
+++ src/main/java/ga/patrick/smns/service/TemperatureService.java
@@ -0,0 +1,22 @@
+package ga.patrick.smns.service;
+
+import ga.patrick.smns.domain.*;
+import ga.patrick.smns.repository.*;
+import lombok.AllArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class TemperatureService {
+
+    private TemperatureRepository temperatureRepository;
+
+    public List<Temperature> lastInputs(int n) {
+        return temperatureRepository.findByOrderByIdDesc(
+                PageRequest.of(0, n)
+        );
+    }
+}
